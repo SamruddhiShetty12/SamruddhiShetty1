cd25969a4378b7935877f627b8772a6af2bc6846
SamruddhiShetty12 <shettysamruddhi12@gmail.com>
2024-09-29 21:44:01 +0530

    Add files via upload
    Front End Web Development

---
+++ myscript.js
@@ -0,0 +1,10 @@
+alert('welcome to FITNESS FREAK')
+var age=prompt('what is your age')
+alert('Thank you for confirming your age as '+  age)
+if (age>6){
+    alert('Thats Great! Continue your journey');
+} else {
+    alert('Sorry This may not be good for you kid!');
+    }
+
+
---
+++ page2.js
@@ -0,0 +1,5 @@
+
+function func1()
+{
+    $("#div1").slideUp(3000);
+}
